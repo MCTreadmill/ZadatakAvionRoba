76386aecb6c78f9ef6d7b7be0dd9ee0b37a4c438
Luka Stojic <luka@arthaus.info>
2017-10-02 20:43:43 +0200

    Uradjen zadatak izmena vrednosti fixes #8

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,17 +1,37 @@
 package zadaci;
 
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.*;
+
 import java.sql.SQLException;
+import java.util.List;
 
 public class Zadatak3IzmenaVrednosti {
+    static Dao<Roba,Integer> robaDao;
 
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
 
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+            List<Roba> robaZaIzmenu = robaDao.queryForEq(Roba.POLJE_OPIS, "Plasticna stolica");
+            Roba roba1 = robaZaIzmenu.get(0);
+            roba1.setOpis("Drvena stolica");
+            robaDao.update(roba1);
+
+            List<Roba> robe = robaDao.queryForAll();
+            for (Roba r : robe)
+                System.out.println(r);
+
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
