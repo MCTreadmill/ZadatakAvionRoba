d0e8c919d4595a4f7186a023a17cfa34895c64df
Luka Stojic <luka@arthaus.info>
2017-10-02 19:02:23 +0200

    Kreirane klase za zadatke fixes #3

---
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -0,0 +1,29 @@
+package zadaci;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Zadatak1KreiranjeTabela {
+
+    public static void main(String[] args) {
+
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:avionRoba.db");
+
+            System.out.println("Uspesno konektovano na bazu");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
